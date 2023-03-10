8b93b96bd4b78c6305e6f025c42802a8f4100ca1
GilKlein <giljklein@gmail.com>
2023-03-10 05:35:51 -0800

    Note added to main App function.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+// Main App function
+
 const App = () => {
 	const expenses = [
 		{
